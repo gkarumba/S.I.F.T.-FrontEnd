eb533ae269b2b8c9edecb0e5094f7e6ce2cd01de
MarkMwago2 <markmwago@gmail.com>
2019-08-22 04:18:11 +0300

    add both registration and login forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from  './material/material.module';
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    MaterialModule
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot ([
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: 'register', component: RegisterComponent},
+      { path: 'login', component: LoginComponent},
+      ]),
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
